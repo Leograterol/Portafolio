4ca62691d636bac23b1a03e8502f22f657bdfbb4
Leonardo <leonardo150490@gmail.com>
2022-07-05 20:59:28 -0400

    adding tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
-    $("a").on('click', function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var hash = this.hash;
-  
-        $('html, body').animate(
-          {
-            scrollTop: $(hash).offset().top-80
-          },
-          700);
-      } 
-    });
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 80
+        },
+        700);
+    }
   });
-  
+});
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
